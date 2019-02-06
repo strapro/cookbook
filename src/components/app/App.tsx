1f0fa4b3a49aa34fa5d7930696347541d47fffae
sprovato <sprovato@informatica.com>
2019-02-06 14:13:53 +0200

    minor refactor

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Template from './App.jsx'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <Template/>;
+  }
+}
+
+export default App;
