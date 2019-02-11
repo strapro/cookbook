d1bb078fe5105997af59f6c4ee1e4d3d4e84d353
sprovato <sprovato@informatica.com>
2019-02-11 12:58:44 +0200

    Moved reciped to component state

--- src/components/recipe-card/RecipeCard.view.jsx
+++ src/components/recipe-card/RecipeCard.view.jsx
@@ -20,28 +20,25 @@ export default function template() {
     <Card className={styles.card}>
       <CardHeader
         avatar={
-          <Avatar aria-label="Recipe" className={styles.avatar}>
-            R
-            </Avatar>
+          <Avatar aria-label="Recipe" src={this.state.recipe.avatar} className={styles.avatar}/>
         }
         action={
           <IconButton>
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={this.state.recipe.title}
+        subheader={this.state.recipe.subheader}
       />
       <CardMedia
         className={styles.media}
-        image="paella.jpg"
-        title="Paella dish"
+        image={this.state.recipe.image}
+        title={this.state.recipe.title}
       />
       <CardContent>
         <Typography component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-          </Typography>
+          {this.state.recipe.description}
+        </Typography>
       </CardContent>
       <CardActions className={styles.actions} disableActionSpacing>
         <IconButton aria-label="Add to favorites">
@@ -63,29 +60,9 @@ export default function template() {
       </CardActions>
       <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-            </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-            chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-            salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-            minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-            to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-            cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
+          {this.state.recipe.steps.map((step, index) => {
+            return <Typography key={index} paragraph>{step}</Typography>
+          })}
         </CardContent>
       </Collapse>
     </Card>
