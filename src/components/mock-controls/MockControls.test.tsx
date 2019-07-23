52e9d1b96b49ad9a0796698cef463ef42d2787eb
strapro <strapro@gmail.com>
2019-07-23 22:40:32 +0300

    Test actually tests something

--- src/components/mock-controls/MockControls.test.tsx
+++ src/components/mock-controls/MockControls.test.tsx
@@ -1,22 +1,46 @@
 import React, {createRef} from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import MockControls from './MockControls';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe('MockControls', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
 
-    let emptyFunction = () => {},
-        element: MockControls = createRef<MockControls>().current!;
+        let emptyFunction = () => {};
 
-    ReactDOM.render(
-        <MockControls
-            onGetHtmlStart={emptyFunction}
-            onGetHtmlComplete={emptyFunction}
-            onGetHtmlError={emptyFunction}
-            ref={ref => element = ref!} />,
-        div);
+        ReactDOM.render(
+            <MockControls
+                onGetHtmlStart={emptyFunction}
+                onGetHtmlComplete={emptyFunction}
+                onGetHtmlError={emptyFunction} />,
+            div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 
-    element.handlePopulateRandomClick('microdata');
+    it('handlePopulateRandomClick and clicking on button populates the input', () => {
+        const div = document.createElement('div');
 
-    ReactDOM.unmountComponentAtNode(div);
+        let emptyFunction = () => {},
+            element: MockControls = createRef<MockControls>().current!;
+
+        ReactDOM.render(
+            <MockControls
+                onGetHtmlStart={emptyFunction}
+                onGetHtmlComplete={emptyFunction}
+                onGetHtmlError={emptyFunction}
+                ref={ref => element = ref!} />,
+            div);
+
+        element.handlePopulateRandomClick('microdata');
+        const input1 = div.querySelector<HTMLInputElement>('input[type="text"]')!.value;
+        expect(input1).not.toBeNull();
+
+        const button = div.querySelector<HTMLElement>('div.secondary button')!;
+        ReactTestUtils.Simulate.click(button);
+        const input2 = div.querySelector<HTMLInputElement>('input[type="text"]')!.value;
+        expect(input2).not.toBeNull();
+
+        expect(input1).not.toStrictEqual(input2);
+    });
 });
