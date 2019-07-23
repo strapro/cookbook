ec8a3992d1760fbf08f505dbec36cdead7fe2715
strapro <strapro@gmail.com>
2019-07-23 22:01:40 +0300

    Played around with tests

--- src/components/mock-controls/MockControls.test.tsx
+++ src/components/mock-controls/MockControls.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import ReactDOM from 'react-dom';
 import MockControls from './MockControls';
 
+let emptyFunction = () => {};
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<MockControls />, div);
+
+    let element: MockControls = createRef<MockControls>().current!;
+
+    ReactDOM.render(
+        <MockControls
+            onGetHtmlStart={emptyFunction}
+            onGetHtmlComplete={emptyFunction}
+            onGetHtmlError={emptyFunction}
+            ref={ref => element = ref!} />,
+        div);
+
+    element.handlePopulateRandomClick('microdata');
+
     ReactDOM.unmountComponentAtNode(div);
 });
